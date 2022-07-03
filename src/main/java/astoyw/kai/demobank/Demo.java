7c9df330dfd4aec5b6be0839c056f72d93930a16
陈凯 <chenkai@asto-inc.com>
2022-07-03 15:09:47 +0800

    dev v1

--- src/main/java/astoyw/kai/demobank/Demo.java
+++ src/main/java/astoyw/kai/demobank/Demo.java
@@ -5,5 +5,9 @@ package astoyw.kai.demobank;
  * @date 2022年07月03日 14:28
  */
 public class Demo {
-
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.print(i+",");
+        }
+    }
 }
