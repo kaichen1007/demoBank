5fac26a48067ecfd11f3f64a1e0d29c0c58c559c
陈凯 <chenkai@asto-inc.com>
2022-07-03 21:55:30 +0800

    测试模版

--- src/main/java/astoyw/kai/demobank/Demo1.java
+++ src/main/java/astoyw/kai/demobank/Demo1.java
@@ -2,9 +2,9 @@ package astoyw.kai.demobank;
 
 /**
  * @author chenKai
- * @date 2022年07月03日 14:28
+ * Date: 2022年07月03日 14:28
  */
-public class Demo {
+public class Demo1 {
     public static void main(String[] args) {
         for (int i = 0; i < 20; i++) {
             System.out.print(i+",");
---
+++ src/main/java/astoyw/kai/demobank/Demo2.java
@@ -0,0 +1,8 @@
+package astoyw.kai.demobank;
+
+/**
+ * @author chenKai
+ * Date: 2022年07月03日 21:55
+ */
+public class Demo2 {
+}
